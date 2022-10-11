f68b693e2dcf2d1f6f079c7d8c41b314cb1ec039
Jayashakthi Vishnu <vishnu28.js@gmail.com>
2022-10-11 21:57:58 +0530

    initialized common connection

--- development-phase/backend/utils/dbQuery.py
+++ development-phase/backend/utils/dbQuery.py
@@ -1,9 +1,10 @@
 import ibm_db
-from .dbConfig import getDbCred
+from utils.dbConfig import getDbCred
+
+conn=ibm_db.connect(getDbCred(),"","")
 
 def selectQuery(query,params=None):
     try:
-        conn=ibm_db.connect(getDbCred(),"","")
         stmt=ibm_db.prepare(conn,query)
         if(params==None):
             ibm_db.execute(stmt)
@@ -14,16 +15,11 @@ def selectQuery(query,params=None):
         return data
     except: 
         return False
-    finally:
-        ibm_db.close(conn)
 
 def insertQuery(query,params):
     try:
-        conn=ibm_db.connect(getDbCred(),"","")
         stmt=ibm_db.prepare(conn,query)
         ibm_db.execute(stmt,params)
         return True
     except:
         return False
-    finally:
-        ibm_db.close(conn)
