c7cacbfa62d568053cc19d6f27c891e2bf03d5c2
Jayashakthi Vishnu <vishnu28.js@gmail.com>
2022-10-11 21:56:20 +0530

    modified import statements

--- development-phase/backend/routes/checkEmail.py
+++ development-phase/backend/routes/checkEmail.py
@@ -1,13 +1,12 @@
 from flask import request
 from flask_restful import Resource
-from utils.dbQuery import *
+from utils.dbQuery import selectQuery
 
 class CheckEmail(Resource):
     def post(self):
         data=request.json
         email=str(data["email"])
-        a=tuple(map(int,email.split(" ")))
-        res=selectQuery("SELECT email FROM USER WHERE EMAIL=?",a)
+        res=selectQuery("SELECT email FROM USER WHERE EMAIL=?",(email,))
         print(res)
         if(not res):
             return {"status":True},200
