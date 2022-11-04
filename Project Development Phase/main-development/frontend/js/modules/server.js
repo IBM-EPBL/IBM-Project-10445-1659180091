90ab699b0ba6edb2f62919011b3d5986e1a51411
Jayashakthi Vishnu <vishnu28.js@gmail.com>
2022-11-04 11:22:13 +0530

    add getter function

--- Project Development Phase/main-development/frontend/js/modules/server.js
+++ Project Development Phase/main-development/frontend/js/modules/server.js
@@ -14,3 +14,12 @@ export const poster=async(endpoint,data)=>{
     return retData;
 }
 
+export const getter=async(endpoint)=>{
+    let url=`http://127.0.0.1:5000/${endpoint}`
+    let retData=await fetch(url,{
+        credentials:"include"
+    });
+    retData=await retData.json();
+    return retData;
+}
+
