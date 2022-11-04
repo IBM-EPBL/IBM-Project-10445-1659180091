a43dce28dae5235d4b6e8c86e4e0f4d7353e8f82
Jayashakthi Vishnu <vishnu28.js@gmail.com>
2022-11-04 11:20:25 +0530

    remove smtp module

--- Project Development Phase/main-development/backend/app.py
+++ Project Development Phase/main-development/backend/app.py
@@ -2,7 +2,6 @@ from dotenv import dotenv_values
 from apiFetch import *
 from flask import Flask
 from flask_cors import CORS
-import smtplib
 from routes.register import Register
 from routes.checkEmail import CheckEmail
 from routes.verify import Verify
@@ -13,23 +12,20 @@ from utils.apiFetch import apiRunner
 
 
 app = Flask(__name__)
-api=Api(app)
+api = Api(app)
 
-data=dotenv_values(".env")
-app.config['SECRET_KEY']="Wonderful secret key"
-app.config['SECURITY_PASSWORD_SALT']="salt passconfirm_token word"
+data = dotenv_values(".env")
+app.config['SECRET_KEY'] = data["secret_key"]
+app.config['SECURITY_PASSWORD_SALT'] = data["secured_password_salt"]
 
 apiRunner()
 
-s = smtplib.SMTP('smtp.gmail.com',587)
-s.starttls()
-s.login("freeacc602@gmail.com",data["email_password"])
 
 CORS(app, supports_credentials=True)
 
-api.add_resource(Login,'/login')
-api.add_resource(CheckEmail,'/register/check')
-api.add_resource(Register,'/register')
-api.add_resource(Verify,'/register/verify')
-api.add_resource(Personal,'/news/recommended')
-api.add_resource(News,'/news/<topic>')
+api.add_resource(Login, '/login')
+api.add_resource(CheckEmail, '/register/check')
+api.add_resource(Register, '/register')
+api.add_resource(Verify, '/register/verify')
+api.add_resource(Personal, '/news/recommended')
+api.add_resource(News, '/news/<topic>')
