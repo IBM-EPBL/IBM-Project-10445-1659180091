50a97ee525b218cf9a5022022d43f196eff15664
Jayashakthi Vishnu <vishnu28.js@gmail.com>
2022-11-06 14:58:08 +0530

    add ip send logic on each request using jsonip api

--- Project Development Phase/main-development/frontend/js/modules/server.js
+++ Project Development Phase/main-development/frontend/js/modules/server.js
@@ -1,11 +1,20 @@
+const ipAddr=async()=>{
+    let url=`https://jsonip.com/`
+    let data=await fetch(url)
+    data=await data.json();
+    data=data["ip"];
+    return data;
+}
 export const poster=async(endpoint,data)=>{
     let url=`http://127.0.0.1:5000/${endpoint}`
+    let ip=await ipAddr();
     let retData=await fetch(url,{
         method:"POST",
         mode:"cors",
         headers:{
             "Access-Control-Allow-Origin":"*",
             "content-type":"application/json",
+            "ip":ip
         },
         body:JSON.stringify(data),
         credentials:"include"
@@ -16,7 +25,11 @@ export const poster=async(endpoint,data)=>{
 
 export const getter=async(endpoint)=>{
     let url=`http://127.0.0.1:5000/${endpoint}`
+    let ip=await ipAddr();
     let retData=await fetch(url,{
+        headers:{
+            "ip":ip
+        },
         credentials:"include"
     });
     retData=await retData.json();
