177a0b957b02006f1a6316ab9bf3b337b9eab32d
Jayashakthi Vishnu <vishnu28.js@gmail.com>
2022-11-10 11:23:51 +0530

    removed logging the browser status

--- Project Development Phase/main-development/frontend/js/modules/server.js
+++ Project Development Phase/main-development/frontend/js/modules/server.js
@@ -2,7 +2,6 @@ const browserData=async()=>{
     let t1=JSON.stringify(window.navigator.userAgentData)
     let t2=window.navigator.userAgent
     let data=t1+t2;
-    console.log(data)
     return data;
 }
 URL=`https://159.122.178.87:30646`
