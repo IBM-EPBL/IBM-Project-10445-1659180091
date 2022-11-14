4f68fd197d037d9e021261ba2edfacbad491aef2
Jayashakthi Vishnu <vishnu28.js@gmail.com>
2022-11-14 20:37:00 +0530

    add bookmark and logout route

--- Project Development Phase/main-development/backend/app.py
+++ Project Development Phase/main-development/backend/app.py
@@ -8,9 +8,10 @@ from routes.verify import Verify
 from routes.getnews import *
 from routes.islogin import IsLogin
 from routes.login import Login
+from routes.logout import LogOut
+from routes.bookmarks import Bookamark,UnBookMark
 from flask_restful import Api
 from utils.apiFetch import apiRunner
-
 
 app = Flask(__name__)
 api = Api(app)
@@ -31,3 +32,6 @@ api.add_resource(Register, '/register')
 api.add_resource(Verify, '/register/verify')
 api.add_resource(Personal, '/news/recommended')
 api.add_resource(News, '/news/<topic>')
+api.add_resource(LogOut,'/logout')
+api.add_resource(Bookamark,"/bookmark")
+api.add_resource(UnBookMark,"/unbookmark")
